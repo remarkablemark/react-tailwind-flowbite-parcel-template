aa6e5762cc84df6d3637da67c1d3dd01e14d25cc
Mark <mark@remarkablemark.org>
2022-10-30 15:29:35 -0400

    feat: add Tailwind CSS with Flowbite
    https://flowbite.com/docs/getting-started/react/
    
    https://flowbite-react.com/

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import './app.css';
+import { Button } from 'flowbite-react';
 
 export default function App() {
-  return <h1>Welcome to React TypeScript Parcel!</h1>;
+  return (
+    <main className="container mx-auto">
+      <h1 className="text-2xl font-medium py-2">
+        Welcome to React Tailwind Flowbite Parcel Template!
+      </h1>
+      <Button onClick={() => console.log('Clicked button')}>Click Me</Button>
+    </main>
+  );
 }
